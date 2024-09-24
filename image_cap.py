08c058e40e64c5d2f7e0a966042d988d44215052
Lampros Velentzas <labrosvel@yahoo.gr>
2024-09-24 20:45:16 +0100

    Intermediary changes

---
+++ image_cap.py
@@ -0,0 +1,7 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
